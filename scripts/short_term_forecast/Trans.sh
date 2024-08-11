cfabc244f50e2cdc1a75ee86ac3a8b87f0be8134
WinstonLiyt <1957922024@qq.com>
2024-08-11 08:37:13 +0000

    change absolute root to relative root

--- scripts/short_term_forecast/Trans.sh
+++ scripts/short_term_forecast/Trans.sh
@@ -5,7 +5,7 @@ model_name=Transformer
 python -u run.py \
   --task_name long_term_forecast \
   --is_training 1 \
-  --root_path /home/liyuante/llm4ts/ticker_csv_files/ \
+  --root_path test_file/ \
   --data_path AAPL.csv \
   --seasonal_patterns 'Monthly' \
   --inverse \
